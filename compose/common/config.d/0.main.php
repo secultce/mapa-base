68031a8123ddaa6575c443b52956a9b96372992a
Wylder Marcos Silva Menezes <41806255+WylderM@users.noreply.github.com>
2022-12-20 13:18:00 -0300

    Ajusta variáveis de ambiente db, email, recaptcha

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -8,18 +8,27 @@ $base_domain = @$_SERVER['HTTP_HOST'];
 $base_url = 'https://' . $base_domain . '/';
 
 return [
-    'app.siteName' => env('SITE_NAME', 'Mapa Cultural do Ceará'),
-    'app.siteDescription' => env('SITE_DESCRIPTION', "O Mapa Cultural do Ceará é a plataforma livre, gratuita e colaborativa de mapeamento da Secretaria da Cultura do Estado do Ceará sobre cenário cultural cearense. Ficou mais fácil se programar para conhecer as opções culturais que as cidades cearenses oferecem: shows musicais, espetáculos teatrais, sessões de cinema, saraus, entre outras. Além de conferir a agenda de eventos, você também pode colaborar na gestão da cultura do estado: basta criar seu perfil de agente cultural. A partir deste cadastro, fica mais fácil participar dos editais e programas da Secretaria e também divulgar seus eventos, espaços ou projetos."),
+    'app.siteName' => env('SITE_NAME', 'Mapa Cultural do CearÃ¡'),
+    'app.siteDescription' => env('SITE_DESCRIPTION', "O Mapa Cultural do CearÃ¡ Ã© a plataforma livre, gratuita e colaborativa de mapeamento da Secretaria da Cultura do Estado do CearÃ¡ sobre cenÃ¡rio cultural cearense. Ficou mais fÃ¡cil se programar para conhecer as opÃ§Ãµes culturais que as cidades cearenses oferecem: shows musicais, espetÃ¡culos teatrais, sessÃµes de cinema, saraus, entre outras. AlÃ©m de conferir a agenda de eventos, vocÃª tambÃ©m pode colaborar na gestÃ£o da cultura do estado: basta criar seu perfil de agente cultural. A partir deste cadastro, fica mais fÃ¡cil participar dos editais e programas da Secretaria e tambÃ©m divulgar seus eventos, espaÃ§os ou projetos."),
 
     'themes.active' => env('ACTIVE_THEME', 'Ceara'),
 
     'app.lcode' => env('APP_LCODE', 'pt_BR'),
 
     'app.enabled.apps' => false,
-        
-    ## Esse módulo é para configurar a funcionalidade de denúncia e/ou sugestões
-    'google-recaptcha-sitekey' => env('GOOGLE_RECAPTCHA_SITEKEY', ''),
-    'google-recaptcha-secret' => env('GOOGLE_RECAPTCHA_SECRET', ''),
+    
+    'plugins.enabled' => array('agenda-singles', 'endereco', 'notifications', 'em-cartaz', 'mailer'),
+
+    'mailer.user' => env('MAILER_USER'),
+    'mailer.psw' => env('MAILER_PASS'),
+    'mailer.server' => env('MAILER_SERVER'),
+    'mailer.protocol' => null,
+    'mailer.port'   => env('MAILER_PORT'),
+    'mailer.from' => env('MAILER_FROM'),
+    
+    ## Esse mÃ³dulo Ã© para configurar a funcionalidade de denÃºncia e/ou sugestÃµes
+    'google-recaptcha-sitekey' => env('GOOGLE_RECAPTCHA_SITEKEY'),
+    'google-recaptcha-secret' => env('GOOGLE_RECAPTCHA_SECRET'),
 
     'namespaces' => array(
         'MapasCulturais\Themes' => THEMES_PATH,
@@ -33,4 +42,14 @@ return [
         'suggestion' => true,
 
     ],
+	
+     'doctrine.database' => [
+         'dbname'    => env('NAME_DB'),
+    	 'user'      => env('USER_DB'),
+    	 'password'  => env('PASSWORD_DB'),
+    	 'host'      => env('HOST_DB'),
+    	 'port'      => env('PORT_DB'),  
+
+     ],
+	
 ];
