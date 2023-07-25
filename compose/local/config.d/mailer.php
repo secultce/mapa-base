103466bd115dacbb75a3de304d6ba3272367a588
Junior-Oliveira <franciscoanto@gmail.com>
2023-07-25 01:00:47 -0300

    :wrench: Add configuração para emails de testes
    Add mais indices no e-mail para disparos de emails locais

--- compose/local/config.d/mailer.php
+++ compose/local/config.d/mailer.php
@@ -1,4 +1,5 @@
 <?php
+use \MapasCulturais\i;
 
 return [
     'mailer.user'       => '',
@@ -10,4 +11,18 @@ return [
 
     // se quiser redirecionar o envio de todos os emails para um mesmo endereço
     'mailer.alwaysTo'   => false,
+    'mailer.templates' => [
+        'welcome' => [
+            'title' => i::__("Bem-vindo(a) ao Mapas Culturais"),
+            'template' => 'welcome.html'
+        ], 
+        'start_registration' => [
+            'title' => i::__("Inscrição iniciada"),
+            'template' => 'start_registration.html'
+        ],
+        'send_registration' => [
+            'title' => i::__("Inscrição enviada"),
+            'template' => 'send_registration.html'
+        ],
+    ]
 ];
